81164908e11081bbb8c6df4a127f85799c5b8fbd
Juan Martin <martintabordagarcia@gmail.com>
2020-07-16 19:19:47 -0500

    se adiciona hola lindos

---
+++ imprimir/imprimir/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace imprimir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola lindos!");
+        }
+    }
+}
